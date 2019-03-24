8b44266d904fe310c50bde183126d9968577fe3a
Yoko Harada <yokolet@gmail.com>
2019-03-23 21:08:55 -0400

    chore: changes variable name
    The vairable name was changed from center to left to fit with its role.

--- lib/arrays_strings/count_palindromes.rb
+++ lib/arrays_strings/count_palindromes.rb
@@ -3,21 +3,21 @@ class CountPalindromes
   # @return {Integer}
   def count_substrings(s)
     return 0 if s.nil? || s.empty?
-    result, center = 0, 0
-    while center < s.size
-      right = center + 1
-      while right < s.size && s[right] == s[center]
+    result, left = 0, 0
+    while left < s.size
+      right = left + 1
+      while right < s.size && s[right] == s[left]
         right += 1
       end
-      result += (right - center) * (right - center + 1) / 2
-      l = center - 1
+      result += (right - left) * (right - left + 1) / 2
+      l = left - 1
       r = right
       while l >= 0 && r < s.size && s[l] == s[r]
         l -= 1
         r += 1
         result += 1
       end
-      center = right
+      left = right
     end
     result
   end
